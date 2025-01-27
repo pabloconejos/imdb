94a21569018638c7f0561a97c6991247c41a5b23
Pablo Conejos <126771457+pabloconejos@users.noreply.github.com>
2025-01-28 00:19:34 +0100

    Delete src/environment.ts

--- src/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'https://api.themoviedb.org/3',
-    apiKeyAuth: '5b96ade3256530f610f6514d29e05ed3',
-    accesTokenAuth: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Yjk2YWRlMzI1NjUzMGY2MTBmNjUxNGQyOWUwNWVkMyIsIm5iZiI6MTczMjgwNzAzOS4yNTEwNjQ4LCJzdWIiOiI2NzIyNWNmOTFkMGI1NzFiOGE1YzI2MDkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ukkw6y1bGgPwXBkxlBCQHx8zw2bpIeAD0SoFmJycvTA'
-}
